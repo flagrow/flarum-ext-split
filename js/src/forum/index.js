24294dd92efb3e42bded677a8aa1722ad7fdf9be
David Wheatley <hi@davwheat.dev>
2021-07-13 00:09:51 +0100

    Various changes

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,15 +1,22 @@
-import Model from 'flarum/common/Model';
+import Model from "flarum/common/Model";
 
-import addSplitControl from './addSplitControl';
-import SplitController from './components/SplitController';
-import DiscussionSplit from './components/DiscussionSplit';
+import addSplitControl from "./addSplitControl";
+import SplitController from "./components/SplitController";
+import DiscussionSplit from "./components/DiscussionSplit";
 
-app.initializers.add('fof-split', (app) => {
-    app.store.models.discussions.prototype.canSplit = Model.attribute('canSplit');
+app.initializers.add("fof-split", (app) => {
+    window.app.__fof_split = {
+        splitting: false,
+        showSplitTos: {},
+        splitController: null,
+    };
+
+    window.app.__fof_split.splitController = new SplitController();
+
+    app.store.models.discussions.prototype.canSplit =
+        Model.attribute("canSplit");
 
     app.postComponents.discussionSplit = DiscussionSplit;
 
-    const splitController = new SplitController();
-
-    addSplitControl(splitController);
+    addSplitControl();
 });
